87b2fd5a1c7feab79c915870ea3a8e61385a0556
Prithveesh Goel <Prithveesh.Goel@go-mmt.com>
2020-07-12 23:45:48 +0530

    Base Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Welcome to MakeMyTrip React Training!!!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Tutorials
         </a>
       </header>
     </div>
