469f704c7dee90c36ecd826d72d17b08b5b0531e
Prithveesh Goel <Prithveesh.Goel@go-mmt.com>
2020-07-14 18:43:51 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useCallback } from 'react';
+import { Switch, Route, Link, withRouter, useHistory, useLocation } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import logo from './logo.png';
 import TaskList from './components/Tasklist/TaskList';
 import AddTask from './components/AddTask/AddTask';
 import './App.css';
 
+const routes = [
+  {
+    path: '/addTask',
+    component: AddTask,
+    exact: true
+  },
+  {
+    path: '/list',
+    component: () => { return (<TaskList title={'This is the title'} theme={'red'} />)},
+  },
+]
 
-
-
-class App extends Component {
-  constructor() {
-    super();
+class AppClass extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       taskListTitle: "This is Task List title"
     }
     setTimeout(() => {
-      this.setState({
-        taskListTitle: "New Title"
-      })
+      console.log("from arrow function: ", this);
+      // this.setState({
+      //   taskListTitle: "New Title"
+      // })
     }, 2000);
+    setTimeout(function() {
+      console.log("from function: ", this);
+    }, 2000);
+    setTimeout(function() {
+      console.log("from bind function: ", this);
+    }.bind(this), 2000);
+  }
+
+  handleRouting = (url) => {
+    const { history, location } = this.props;
+    // console.log(location.pathname);
+    if(location.pathname === '/') {
+      history.push(url);
+    }
   }
 
   render() {
@@ -31,12 +57,59 @@ class App extends Component {
           <h1>
             Welcome to MakeMyTrip React Training!!!
           </h1>
-          <AddTask />
-          <TaskList title={taskListTitle} theme={'red'} />
+          <ul>
+            <li onClick={() => this.handleRouting("/addTask")}>Add Task</li>
+            <li onClick={() => this.handleRouting("/list")}>Task List</li>
+          </ul>
+          {/* <Switch>
+            <Route path="/addTask" component={AddTask}></Route>
+            <Route 
+              path="/list" 
+              component={() => { return (<TaskList title={taskListTitle} theme={'red'} />)}}
+            >
+            </Route>
+          </Switch> */}
+          {renderRoutes(routes)}
         </header>
       </div>
     );
   }
 }
 
+function App(props) {
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleRouting = useCallback((url) => {
+    console.log(location.pathname);
+    history.push(url);
+  }, [history, location]);
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to MakeMyTrip React Training!!!
+        </h1>
+        <ul>
+          <li onClick={() => handleRouting("/addTask")}>Add Task</li>
+          <li onClick={() => handleRouting("/list")}>Task List</li>
+        </ul>
+        <Switch>
+          <Route path="/addTask" component={AddTask}></Route>
+          <Route 
+            path="/list" 
+            component={() => { return (<TaskList title={'this is a title'} theme={'red'} />)}}
+          >
+          </Route>
+        </Switch>
+        {/* {renderRoutes(routes)} */}
+      </header>
+    </div>
+  );
+}
+
+// export default withRouter(App);
 export default App;
--- src/components/Tasklist/TaskList.js
+++ src/components/Tasklist/TaskList.js
@@ -1,79 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState, useCallback } from 'react';
 import TaskItem from '../TaskItem/TaskItem';
 
-class TaskList extends Component {
+const getData = async () => {
+    const listOfApisResponse = await fetch("https://swapi.dev/api/");
+    // listOfApisResponse
+    // .then((response) => {
+    //     console.log("response", response);
+    //     const jsonPromise = response.json();
+    //     return jsonPromise;
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
+    console.log("promise", listOfApisResponse);
+    const listOfApis = await listOfApisResponse.json();
+    console.log(listOfApis);
+    setTimeout(() => {
+        console.log('timeout');
+    });
+    const filmsPromise = await fetch(listOfApis.films);
+    const films = await filmsPromise.json();
+    console.log(films);
+};
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            activated: 1,
-        };
-    }
 
-    componentDidMount() {
-        this.initiateData();
-        console.log('did mount');
-    }
+function TaskList(props) {
+    const [activated, setActivated] = useState(1);
 
-    async initiateData() {
-        await this.getData();
+    const initiateData = useCallback(async () => {
+        await getData();
         console.log("Data Fetched");
-    }
+    }, []); // <- either values which getting retrieved from props, or a state value, or a function created through useCallback/useMemo
 
-    async getData() {
-        const listOfApisResponse = await fetch("https://swapi.dev/api/");
-        // listOfApisResponse
-        // .then((response) => {
-        //     console.log("response", response);
-        //     const jsonPromise = response.json();
-        //     return jsonPromise;
-        // })
-        // .then(response => {
-        //     console.log(response);
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        // });
-        console.log("promise", listOfApisResponse);
-        const listOfApis = await listOfApisResponse.json();
-        console.log(listOfApis);
-        setTimeout(() => {
-            console.log('timeout');
-        });
-        const filmsPromise = await fetch(listOfApis.films);
-        const films = await filmsPromise.json();
-        console.log(films);
-    }
+    useEffect(() => {
+        // this is equivalent to did mount
+        // initiateData();
+        console.log('did mount');
+    }, [initiateData]);
 
-    activateTask = (id) => {
-        // this.setState({
-        //     activated: id
-        // });
-        console.log(id);
-    }
+    useEffect(() => {
+        initiateData();
+        console.log(activated);
+    }, [initiateData, activated]);
 
-    handleClick = (event) => {
-        console.log(this);
+    const activateTask = useCallback((id) => {
+        setActivated(id);
+    }, []);
+
+    const handleClick = useCallback((event) => {
         console.log(event);
-    }
+    }, []);
 
-    render() {
-        const { activated } = this.state;
-        const { title, theme } = this.props;
-        return (
+    const { title, theme } = props;
+    return (
+        <div>
+            <div onClick={handleClick}>{title}</div>
             <div>
-                <div onClick={this.handleClick}>{title}</div>
-                <div>
-                    <TaskItem id={1} taskClicked={this.activateTask} theme={activated === 1 ? theme : 'white'} />
-                    <TaskItem id={2} taskClicked={this.activateTask} theme={activated === 2 ? theme : 'white'} />
-                    <TaskItem id={3} taskClicked={this.activateTask} theme={activated === 3 ? theme : 'white'} />
-                    <TaskItem id={4} taskClicked={this.activateTask} theme={activated === 4 ? theme : 'white'} />
-                    <TaskItem id={5} taskClicked={this.activateTask} theme={activated === 5 ? theme : 'white'} />
-                </div>
+                <TaskItem id={1} taskClicked={activateTask} theme={activated === 1 ? theme : 'white'} />
+                <TaskItem id={2} taskClicked={activateTask} theme={activated === 2 ? theme : 'white'} />
+                <TaskItem id={3} taskClicked={activateTask} theme={activated === 3 ? theme : 'white'} />
+                <TaskItem id={4} taskClicked={activateTask} theme={activated === 4 ? theme : 'white'} />
+                <TaskItem id={5} taskClicked={activateTask} theme={activated === 5 ? theme : 'white'} />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default TaskList;
